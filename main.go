c44b329406ffc1253bc3d4ec1ea46e972bf463fa
Shabix <2936670268@qq.com>
2025-09-21 16:06:45 +0800

    feat(路由): 添加文件上传接口
    新增FilesController处理文件上传功能，支持将上传的文件保存到uploads目录并返回访问URL

--- main.go
+++ main.go
@@ -36,6 +36,7 @@ func main() {
 		r.GET("/uploads/:path", UploadsController)
 		r.GET("/api/v1/addresses", AddressController)
 		r.GET("/api/v1/qrcodes", QRCodesController)
+		r.POST("/api/v1/files", FilesController)
 		staticFiles, _ := fs.Sub(FS, "frontend/dist")
 		r.StaticFS("/static", http.FS(staticFiles)) // 在路由中添加静态文件前端服务
 		// 所有未匹配的路由都返回index.html
@@ -204,4 +205,31 @@ func QRCodesController(c *gin.Context) {
 	} else {
 		c.Status(http.StatusBadRequest)
 	}
+}
+
+func FilesController(c *gin.Context) {
+	file, err := c.FormFile("raw")
+	if err != nil {
+		log.Fatal(err)
+	}
+	exe, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dir := filepath.Dir(exe)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filename := uuid.New().String()
+	uploads := filepath.Join(dir, "uploads")
+	err = os.MkdirAll(uploads, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fullpath := path.Join("uploads", filename+filepath.Ext(file.Filename))
+	fileErr := c.SaveUploadedFile(file, filepath.Join(dir, fullpath))
+	if fileErr != nil {
+		log.Fatal(fileErr)
+	}
+	c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath})
 }
