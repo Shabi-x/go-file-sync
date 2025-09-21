e93372835ec547dc878e107ab019daf9061b9e8d
Shabix <2936670268@qq.com>
2025-09-21 13:23:13 +0800

    feat(路由): 添加文件下载接口
    新增/uploads/:path GET路由用于文件下载功能，支持从uploads目录获取文件并作为附件返回

--- main.go
+++ main.go
@@ -32,6 +32,7 @@ func main() {
 			c.String(http.StatusOK, "<html><head><title>go-file-sync</title></head><body><h1>Hello, Gin!</h1><p>This is served by Gin on port 8080</p></body></html>")
 		})
 		r.POST("/api/v1/texts", TextController)
+		r.GET("/uploads/:path", UploadsController)
 		r.GET("/api/v1/addresses", AddressController)
 		staticFiles, _ := fs.Sub(FS, "frontend/dist")
 		r.StaticFS("/static", http.FS(staticFiles)) // 在路由中添加静态文件前端服务
@@ -142,4 +143,42 @@ func AddressController(c *gin.Context) {
 		}
 	}
 	c.JSON(http.StatusOK, gin.H{"addresses": result})
+}
+
+func getUploadDir() (uploads string) {
+	exe, err := os.Executable() // 获取当前可执行文件的路径,并绑定到uploads目录下
+	if err != nil {
+		log.Fatal("get executable path failed", err)
+	}
+	dir := filepath.Dir(exe) // 获取当前可执行文件exe的目录
+	uploads = filepath.Join(dir, "uploads")
+	return
+}
+
+/**
+ * @api {get} /uploads/:path 文件下载
+ * @apiName DownloadFile
+ * @apiGroup File
+ *
+ * @apiParam {String} path 文件路径（相对路径）
+ *
+ * @apiSuccess {File} file 下载的文件
+ * @apiError {NotFound} 404 文件未找到
+ */
+func UploadsController(c *gin.Context) {
+	// 从URL参数中获取文件路径
+	if path := c.Param("path"); path != "" {
+		// 构建完整的文件路径（上传目录 + 文件名）
+		target := filepath.Join(getUploadDir(), path)
+		// 设置HTTP响应头
+		c.Header("Content-Description", "File Transfer")              // 内容描述：文件传输
+		c.Header("Content-Transfer-Encoding", "binary")               // 传输编码：二进制
+		c.Header("Content-Type", "application/octet-stream")          // 内容类型：二进制流，支持所有文件类型
+		c.Header("Content-Disposition", "attachment; filename="+path) // 内容处置：作为附件下载，并指定文件名
+
+		// 将文件发送给前端
+		c.File(target)
+	} else {
+		c.Status(http.StatusNotFound)
+	}
 }
